10418519ce19a7e7714f79468fabd81bef651f22
Manel <manel.gonzalez.hernandez@gmail.com>
2022-02-04 23:02:09 +0100

    Models and Routes

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var createError  = require('http-errors');
+var express      = require('express');
+var path         = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger       = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('dotenv').config();
+
 var app = express();
+
+const { MongoClient } = require('mongodb');
+const uri    = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err =>
+{
+    console.log('Conexión correcta');
+    client.close();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,15 +35,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next)
+{
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next)
+{
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error   = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
